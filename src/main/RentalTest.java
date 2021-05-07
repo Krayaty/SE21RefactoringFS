f7163fc73ff7751495960c72431290f04fcf46a8
Krayaty <fabian.schwickert@me.com>
2021-05-07 17:04:14 +0200

    Moved amountFor and renamed to getCharge

---
+++ src/main/RentalTest.java
@@ -0,0 +1,117 @@
+package main;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RentalTest {
+
+    private Movie newRelease;
+    private Movie childrens;
+    private Movie regular;
+
+    private ArrayList<Rental> newReleaseRentals;
+    private ArrayList<Rental> childrensRentals;
+    private ArrayList<Rental> regularRentals;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+
+        newRelease = new Movie("Movie1", Movie.NEW_RELEASE);
+        childrens = new Movie("Movie2", Movie.CHILDRENS);
+        regular = new Movie("Movie3", Movie.REGULAR);
+
+        newReleaseRentals = new ArrayList<>();
+        newReleaseRentals.add(new Rental(newRelease, 1));
+        newReleaseRentals.add(new Rental(newRelease, 2));
+        newReleaseRentals.add(new Rental(newRelease, 3));
+        newReleaseRentals.add(new Rental(newRelease, 4));
+
+        childrensRentals = new ArrayList<>();
+        childrensRentals.add(new Rental(childrens, 1));
+        childrensRentals.add(new Rental(childrens, 2));
+        childrensRentals.add(new Rental(childrens, 3));
+        childrensRentals.add(new Rental(childrens, 4));
+
+        regularRentals = new ArrayList<>();
+        regularRentals.add(new Rental(regular, 1));
+        regularRentals.add(new Rental(regular, 2));
+        regularRentals.add(new Rental(regular, 3));
+        regularRentals.add(new Rental(regular, 4));
+    }
+
+    @Test
+    @DisplayName("Tells correct amount of money owed for a given rental of type NEW_RELEASE.")
+    public void testGetChargeNewRelease() {
+
+        double actual;
+        double expected = 3;
+        assertEquals(expected, (actual = newReleaseRentals.get(0).getCharge()),
+                "The amount of money owed for the rental: \"" + newReleaseRentals.get(0) + "\" should be " + expected + "\n" +
+                        "The amount was: " + actual + ".");
+
+    }
+
+    @Test
+    @DisplayName("Tells correct amount of money owed for a given rental of type CHILDRENS.")
+    public void testGetChargeChildrens() {
+
+        double actual;
+        double expected;
+
+        /** Retail within the average-rental-time-bound */
+
+        expected = 1.5;
+
+        assertEquals(expected, (actual = childrensRentals.get(0).getCharge()),
+                "The amount of money owed for the rental: \"" + childrensRentals.get(0) + "\" should be " + expected +
+                        "The amount was: " + actual + ".");
+
+
+        /** Retail outside of the average-rental-time-bound */
+
+        expected = 3.0;
+
+        assertEquals(expected, (actual = childrensRentals.get(3).getCharge()),
+                "The amount of money owed for the rental: \"" + childrensRentals.get(3) + "\" should be " + expected + "\n" +
+                        "The amount was: " + actual + ".");
+
+    }
+
+    @Test
+    @DisplayName("Tells correct amount of money owed for a given rental of type REGULAR.")
+    public void testGetChargeRegular() {
+
+        double actual;
+        double expected;
+
+        /** Retail within the average-rental-time-bound */
+
+        expected = 2;
+
+        assertEquals(expected, (actual = regularRentals.get(0).getCharge()),
+                "The amount of money owed for the rental: \"" + regularRentals.get(0) + "\" should be " + expected + "\n" +
+                        "The amount was: " + actual + ".");
+
+
+        /** Retail outside of the average-rental-time-bound */
+
+        expected = 3.5;
+
+        assertEquals(expected, (actual = regularRentals.get(2).getCharge()),
+                "The amount of money owed for the rental: \"" + regularRentals.get(2) + "\" should be " + expected + "\n" +
+                        "The amount was: " + actual + ".");
+
+        expected = 5.0;
+
+        assertEquals(expected, (actual = regularRentals.get(3).getCharge()),
+                "The amount of money owed for the rental: \"" + regularRentals.get(3) + "\" should be " + expected + "\n" +
+                        "The amount was: " + actual + ".");
+
+    }
+
+}
