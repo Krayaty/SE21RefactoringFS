e196dc4342d37e5d66c4c92b2ddd1d0fac60a42b
Krayaty <fabian.schwickert@me.com>
2021-05-07 08:40:10 +0200

    Project-setup for testing: program into package main
    Adjusted program code for readability

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -1,24 +1,30 @@
+package main;
 
 import java.lang.*;
 import java.util.*;
 
 class Customer {
+
     private String name;
     private Vector rentals = new Vector();
+
     public Customer (String newname){
         name = newname;
     };
+
     public void addRental(Rental arg) {
         rentals.addElement(arg);
     };
+
     public String getName (){
         return name;
     };
+
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();	    
-        String result = "Rental Record for " + this.getName() + "\n";
+        String result = "main.Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
@@ -62,4 +68,3 @@ class Customer {
     }
 
 }
-    
